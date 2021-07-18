acd0c70f08f4d624765d8726deff5f1bfe9dc5f8
kiiru4reals <smaina991@gmail.com>
2021-07-18 21:44:58 +0300

    Dark mode implementation (buggy)

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
-// import 'ui/cart.dart';
-// import 'ui/feeds.dart';
-// import 'ui/home.dart';
-// import 'ui/search.dart';
-// import 'ui/user.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/const/theme_data.dart';
+import 'package:shop/provider/dark_theme_provider.dart';
 import 'ui/bottom_bar.dart';
-// void main() => runApp(ScaffoldExample());
+
+
 void main() => runApp( MaterialApp(
   home: BottomBarScreen(),
 ));
+class MyApp extends StatelessWidget {
+  DarkThemeProvider themeChangeProvider = DarkThemeProvider();
+  @override
+
+  Widget build(BuildContext context) {
+    return MultiProvider (providers: [
+      ChangeNotifierProvider(create: (_){
+        return themeChangeProvider;
+      })
+    ],
+    child:
+    Consumer<DarkThemeProvider>(
+      builder: (context, themeData, child) {
+        return MaterialApp(
+          theme: Styles.themeData(themeChangeProvider.darkTheme, context),
+        );
+      }
+    ));
+  }
+}
