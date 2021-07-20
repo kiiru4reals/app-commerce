495246a890460283d847a5dc2b2e55e55dcab749
kiiru4reals <smaina991@gmail.com>
2021-07-20 12:32:04 +0300

    Dark mode fix

--- lib/provider/dark_theme_provider.dart
+++ lib/provider/dark_theme_provider.dart
@@ -1,12 +1,14 @@
+import 'package:shop/models/dark_theme_preferences.dart';
 import 'package:flutter/material.dart';
 
 class DarkThemeProvider with ChangeNotifier{
-  bool _darkTheme = false;
-  bool get darkTheme => _darkTheme;
+  DarkThemePreferences darkThemePreferences = DarkThemePreferences();
+  bool _darkTheme = false ;
+  bool get darkTheme=>_darkTheme;
 
   set darkTheme (bool value){
     _darkTheme = value;
+    darkThemePreferences.setDarkTheme(value);
     notifyListeners();
   }
-
 }
