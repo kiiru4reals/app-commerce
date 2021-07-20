9b5554bf017e996598e07406362f0a5249cef39e
kiiru4reals <smaina991@gmail.com>
2021-07-20 19:46:19 +0300

    initial commit

--- lib/ui/empty_cart.dart
+++ lib/ui/empty_cart.dart
@@ -1,21 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/const/colors.dart';
+import 'package:shop/provider/dark_theme_provider.dart';
 
-class CartEmpty extends StatelessWidget {
-  const CartEmpty({Key? key}) : super(key: key);
-
+class EmptyCart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final themeChange = Provider.of<DarkThemeProvider>(context);
     return Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+
       children: [
         Container(
+          margin: EdgeInsets.only(top: 80),
           width: double.infinity,
           height: MediaQuery.of(context).size.height*.4,
-          decoration: BoxDecoration(
-            image: DecorationImage(fit: BoxFit.fill,
-                image: AssetImage('assets/images/emptycart.png')
-            )
+          decoration: BoxDecoration(image: DecorationImage(
+              fit: BoxFit.fill,
+              image: AssetImage('images/emptycart.png')
           ),
-        )
+          ),
+        ),
+        Text('Your cart is empty',
+          style: TextStyle(color: Theme.of(context).textSelectionColor,
+            fontSize: 36,
+            fontWeight: FontWeight.w600
+        ),
+        textAlign: TextAlign.center,
+        ),
+        SizedBox(height: 50,),
+        Text('Looks like you did not add \n anything to your cart yet',
+          style: TextStyle(color: themeChange.darkTheme ? Theme.of(context).disabledColor: ColorsConsts.subTitle,
+              fontSize: 26,
+              fontWeight: FontWeight.w600
+          ),
+          textAlign: TextAlign.center,
+        ),
+        SizedBox(
+          height: 50,
+        ),
+        Container(
+
+          width: MediaQuery.of(context).size.width*.9,
+          height: MediaQuery.of(context).size.height*.06,
+          child: ElevatedButton(onPressed: (){},
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16),
+                    side: BorderSide(color: Colors.cyanAccent.shade400)
+                ),
+                primary: Colors.redAccent,
+              ),
+              child: Text('Go shopping'.toUpperCase(),
+                style: TextStyle(color: themeChange.darkTheme ? Theme.of(context).disabledColor: ColorsConsts.subTitle,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600
+                ),
+                textAlign: TextAlign.center,
+              )
+          ),
+        ),
       ],
     );
   }
