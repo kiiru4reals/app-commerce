0d957a4a1dc4155c490d93bffa584e2c1a74659c
kiiru4reals <smaina991@gmail.com>
2021-07-27 12:50:17 +0300

    added feeds_products.dart and staggered grid view

--- lib/ui/feeds.dart
+++ lib/ui/feeds.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:shop/widget/feeds_products.dart';
 
 class Feed extends StatelessWidget {
   const Feed({Key? key}) : super(key: key);
@@ -11,9 +13,24 @@ class Feed extends StatelessWidget {
         title: Text("Our latest products"),
         centerTitle: true
       ),
-      body: Center(
-        child: Text("Feed"),
-      ),
+      body:
+      // StaggeredGridView.countBuilder(
+      //   crossAxisCount: 6,
+      //   itemCount: 8,
+      //   itemBuilder: (BuildContext context, int index) => FeedsProduct(),
+      //   staggeredTileBuilder: (int index) =>
+      //   new StaggeredTile.count(3, index.isEven ? 4 : 5),
+      //   mainAxisSpacing: 8.0,
+      //   crossAxisSpacing: 6.0,
+      // )
+      GridView.count(
+        crossAxisCount: 2,
+      childAspectRatio: 240/290,
+      crossAxisSpacing: 8,
+      mainAxisSpacing: 8,
+      children: List.generate(100, (index){
+        return FeedsProduct();
+      }),)
     );
   }
 }
