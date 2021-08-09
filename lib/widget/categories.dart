2610fb48d9ab8633fe8e582a680b6fbc0c871f39
kiiru4reals <smaina991@gmail.com>
2021-08-09 09:43:56 +0300

    implemented null safety

--- lib/widget/categories.dart
+++ lib/widget/categories.dart
@@ -1,116 +1,121 @@
 import 'package:flutter/material.dart';
+
 class CategoryWidget extends StatefulWidget {
-  CategoryWidget({Key? key, required this.index}) : super(key: key);
-  final int? index;
+  const CategoryWidget({Key? key, required this.index}) : super(key: key);
+  final int index;
 
   @override
-  State<CategoryWidget> createState() => _CategoryWidgetState();
+  _CategoryWidgetState createState() => _CategoryWidgetState();
 }
 
 class _CategoryWidgetState extends State<CategoryWidget> {
-  List<Map<String,Object>> categories=[
+  List<Map<String, Object>> categories = [
     {
       'categoryName': 'Baby products',
-      'categoryImagePath': 'images/baby.jpg'
-    },
-    {
-      'categoryName': 'Breakfast cereals',
-      'categoryImagePath': 'images/breakfast_cereals.jpg',
+      'categoryImagesPath': 'images/baby.jpg',
     },
     {
       'categoryName': 'Beverages',
-      'categoryImagePath': 'images/beverages.jpg'
+      'categoryImagesPath': 'images/beverages.jpg',
     },
     {
-      'categoryName': 'Cooking Fats & Oils',
-      'categoryImagePath': 'images/cooking_fat.jpg'
+      'categoryName': 'Breakfast cereals',
+      'categoryImagesPath': 'images/breakfast_cereals.jpg',
     },
     {
-      'categoryName': 'Dairy',
-      'categoryImagePath': 'images/dairy.jpg'
+      'categoryName': 'Cooking Fat',
+      'categoryImagesPath': 'images/cooking_fat.jpg',
+    },
+    {
+      'categoryName': 'Cooking oils',
+      'categoryImagesPath': 'images/cooking_oil.jpg',
+    },
+    {
+      'categoryName': 'Dairy products',
+      'categoryImagesPath': 'images/dairy.jpg',
     },
     {
       'categoryName': 'Fruits and Vegetables',
-      'categoryImagePath': 'images/f and v.jpg'
+      'categoryImagesPath': 'images/f and v.jpg',
     },
     {
-      'categoryName': 'Flour',
-      'categoryImagePath': 'images/flours.jpg'
+      'categoryName': 'Flours',
+      'categoryImagesPath': 'images/flours.jpg',
     },
     {
-      'categoryName': 'Kitchenware',
-      'categoryImagePath': 'images/kitchen_essentials.jpg'
+      'categoryName': 'Kitchen Essentials',
+      'categoryImagesPath': 'images/kitchen_essentials.jpg',
     },
     {
       'categoryName': 'Laundry',
-      'categoryImagePath': 'images/laundry.jpg'
+      'categoryImagesPath': 'images/laundry.jpg',
     },
     {
-      'categoryName': 'Noodles & spaghetti',
-      'categoryImagePath': 'images/noodles.jpg'
+      'categoryName': 'Noodles',
+      'categoryImagesPath': 'images/noodles.jpg',
     },
     {
       'categoryName': 'Personal effects',
-      'categoryImagePath': 'images/personal_effects.jpg'
+      'categoryImagesPath': 'images/personal_effects.jpg',
     },
     {
-      'categoryName': 'Rice and Cereals',
-      'categoryImagePath': 'images/rice_cereals.jpg'
+      'categoryName': 'Rice and cereals',
+      'categoryImagesPath': 'images/rice_cereals.jpg',
     },
     {
-      'categoryName': 'Sauces & spices',
-      'categoryImagePath': 'images/sauces_spices.jpg'
+      'categoryName': 'Sauces and spices',
+      'categoryImagesPath': 'images/sauces_spices.jpg',
     },
     {
       'categoryName': 'Snacks',
-      'categoryImagePath': 'images/snacks.jpg'
+      'categoryImagesPath': 'images/snacks.jpg',
     },
     {
       'categoryName': 'Spreads',
-      'categoryImagePath': 'images/spreads.jpg'
+      'categoryImagesPath': 'images/spreads.jpg',
     },
     {
       'categoryName': 'Sugar',
-      'categoryImagePath': 'images/sugar.jpg'
+      'categoryImagesPath': 'images/sugar.jpg',
     },
     {
       'categoryName': 'Wellness',
-      'categoryImagePath': 'images/wellness.jpg'
-    }
-
+      'categoryImagesPath': 'images/wellness.jpg',
+    },
   ];
 
   @override
   Widget build(BuildContext context) {
     return Stack(
-
       children: [
         Container(
-          decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),
-          image: DecorationImage(image: AssetImage(categories[widget.index]!['categoryImagePath']),
-            fit: BoxFit.cover
-          ),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            image: DecorationImage(
+              image: AssetImage(categories[widget.index]['categoryImagesPath']?.toString()?? ''),
+            fit: BoxFit.cover),
           ),
           margin: EdgeInsets.symmetric(horizontal: 10),
           width: 150,
           height: 150,
-
-
         ),
         Positioned(
-            bottom:0,
-            left: 10,
-            right: 10,
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 16,
-              vertical: 8),
-              color: Theme.of(context).backgroundColor,
-              child: Text(categories[widget.index]!["categoryName"], style: TextStyle(
+          bottom: 0,
+          left: 10,
+          right: 10,
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+            color: Theme.of(context).backgroundColor,
+            child: Text(
+              categories[widget.index]['categoryName']?.toString()?? '',
+              style: TextStyle(
                 fontWeight: FontWeight.w500,
                 fontSize: 18,
-                color: Theme.of(context).textSelectionColor
-              ),),
-            ))
+                color: Theme.of(context).textSelectionColor,
+              ),
+            ),
+          ),
+        )
       ],
     );
   }
