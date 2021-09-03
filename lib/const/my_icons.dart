26e82d59d40cc25e8ae712dd4d20910af76decaf
kiiru4reals <smaina991@gmail.com>
2021-09-03 17:42:36 +0300

    Functional icons (Material)

--- lib/const/my_icons.dart
+++ lib/const/my_icons.dart
@@ -19,11 +19,9 @@ import 'package:fluttericon/web_symbols_icons.dart';
 import 'package:fluttericon/zocial_icons.dart';
 
 class MyAppIcons {
-
-  static IconData home = Feather.home;
-  static IconData rss = Feather.rss;
-  static IconData search = Feather.search;
-  static IconData user = Feather.user;
-  static IconData cart = MaterialCommunityIcons.cart;
-
+  static IconData home = Icons.home;
+  static IconData rss = Icons.rss_feed_outlined;
+  static IconData search = Icons.search;
+  static IconData user = Icons.person;
+  static IconData cart = Icons.shopping_cart_outlined;
 }
