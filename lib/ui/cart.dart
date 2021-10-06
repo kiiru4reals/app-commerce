f4735a5ebf93f48b49ddddaff26b94b7261b8d76
kiiru4reals <smaina991@gmail.com>
2021-10-06 20:00:58 +0300

    Provider integration

--- lib/ui/cart.dart
+++ lib/ui/cart.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:ionicons/ionicons.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/const/colors.dart';
+import 'package:shop/models/cart_attr.dart';
+import 'package:shop/provider/cart_provider.dart';
 import 'package:shop/widget/cart_full.dart';
 import 'package:shop/widget/empty_cart.dart';
 
@@ -10,25 +13,38 @@ class ShoppingCart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List products = [];
-    return !products.isEmpty? Scaffold(
-      body:  EmptyCart() ,
+    final cartProvider = Provider.of<CartProvider>(context);
 
-    ): Scaffold(
-      bottomSheet: checkoutSection(context),
+    return cartProvider.getCartItems.isEmpty
+        ? Scaffold(body: EmptyCart())
+        : Scaffold(
+        bottomSheet: checkoutSection(context),
         appBar: AppBar(
           title: Text('Cart item'),
           actions: [
             InkWell(
               onTap: (){},
-              child: Icon(Ionicons.trash_bin),
+              child: Icon(Icons.delete),
             ),
           ],
         ),body: Container(
       margin: EdgeInsets.only(bottom: 60),
-          child: ListView.builder(itemCount: 5,
+          child: ListView.builder(
+              itemCount: cartProvider.getCartItems.length,
       itemBuilder: (BuildContext ctx, int index){
-            return CartFull();
+        return ChangeNotifierProvider.value(
+          value: cartProvider.getCartItems.values.toList()[index],
+          child: CartFull(
+            productId:
+            cartProvider.getCartItems.keys.toList()[index],
+            // id:  cartProvider.getCartItems.values.toList()[index].id,
+            // productId: cartProvider.getCartItems.keys.toList()[index],
+            // price: cartProvider.getCartItems.values.toList()[index].price,
+            // title: cartProvider.getCartItems.values.toList()[index].title,
+            // imageUrl: cartProvider.getCartItems.values.toList()[index].imageUrl,
+            // quatity: cartProvider.getCartItems.values.toList()[index].quantity,
+          ),
+        );
       }),
         )
     );
