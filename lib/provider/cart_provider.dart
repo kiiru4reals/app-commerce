089bde62721b6c46cf83fd8fcde52393463bd9be
kiiru4reals <smaina991@gmail.com>
2021-10-06 19:59:22 +0300

    Initial commit

---
+++ lib/provider/cart_provider.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:shop/models/cart_attr.dart';
+
+class CartProvider with ChangeNotifier {
+  Map<String, CartAttr> _cartItems = {};
+
+  Map<String, CartAttr> get getCartItems {
+    return _cartItems;
+  }
+
+  double get totalamount {
+    var total = 0.0;
+    _cartItems.forEach((key, value) {
+      total += value.price * value.quantity;
+    });
+    return total;
+  }
+
+  void addProductToCart(String productId, double price, String title,
+      String imageUrl) {
+    if (_cartItems.containsKey(productId)) {
+      _cartItems.update(productId, (existingCartItem) =>
+          CartAttr(
+              id: existingCartItem.id,
+              title: existingCartItem.title,
+              quantity: existingCartItem.quantity+ 1,
+              price: existingCartItem.price,
+              imageUrl: existingCartItem.imageUrl
+          ));
+    } else{
+        _cartItems.putIfAbsent(productId, () =>
+            CartAttr(
+                id: DateTime.now().toString(),
+                title: title,
+                quantity: 1,
+                price: price,
+                imageUrl: imageUrl
+            ));
+    }
+    notifyListeners();
+  }
+}
