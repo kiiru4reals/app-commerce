0b0f627dbbdb7392ab7f5f3bd4ef805571a79559
kiiru4reals <smaina991@gmail.com>
2021-10-07 11:19:26 +0300

    Updating, reducing and clearing cart item(s)

--- lib/provider/cart_provider.dart
+++ lib/provider/cart_provider.dart
@@ -16,27 +16,53 @@ class CartProvider with ChangeNotifier {
     return total;
   }
 
-  void addProductToCart(String productId, double price, String title,
-      String imageUrl) {
+  void addProductToCart(
+      String productId, double price, String title, String imageUrl) {
     if (_cartItems.containsKey(productId)) {
-      _cartItems.update(productId, (existingCartItem) =>
-          CartAttr(
+      _cartItems.update(
+          productId,
+          (existingCartItem) => CartAttr(
               id: existingCartItem.id,
               title: existingCartItem.title,
-              quantity: existingCartItem.quantity+ 1,
+              quantity: existingCartItem.quantity + 1,
               price: existingCartItem.price,
-              imageUrl: existingCartItem.imageUrl
-          ));
-    } else{
-        _cartItems.putIfAbsent(productId, () =>
-            CartAttr(
-                id: DateTime.now().toString(),
-                title: title,
-                quantity: 1,
-                price: price,
-                imageUrl: imageUrl
-            ));
+              imageUrl: existingCartItem.imageUrl));
+    } else {
+      _cartItems.putIfAbsent(
+          productId,
+          () => CartAttr(
+              id: DateTime.now().toString(),
+              title: title,
+              quantity: 1,
+              price: price,
+              imageUrl: imageUrl));
     }
+    notifyListeners();
+  }
+
+  void reduceItemByOne(String productId) {
+    if (_cartItems.containsKey(productId)) {
+      _cartItems.update(
+          productId,
+          (existingCartItem) => CartAttr(
+              id: existingCartItem.id,
+              title: existingCartItem.title,
+              quantity: existingCartItem.quantity - 1,
+              price: existingCartItem.price,
+              imageUrl: existingCartItem.imageUrl));
+    }
+    notifyListeners();
+  }
+
+  void removeItem(
+    String productId,
+  ) {
+    _cartItems.remove(productId);
+    notifyListeners();
+  }
+
+  void clearCart() {
+    _cartItems.clear();
     notifyListeners();
   }
 }
