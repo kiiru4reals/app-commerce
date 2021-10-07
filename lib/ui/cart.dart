25a6b5ee194efea34e8f536d1dad0a3e2492d3d3
kiiru4reals <smaina991@gmail.com>
2021-10-07 11:17:52 +0300

    Code efficiency- see globalmethod.dart

--- lib/ui/cart.dart
+++ lib/ui/cart.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 import 'package:shop/const/colors.dart';
 import 'package:shop/models/cart_attr.dart';
 import 'package:shop/provider/cart_provider.dart';
+import 'package:shop/services/global_method.dart';
 import 'package:shop/widget/cart_full.dart';
 import 'package:shop/widget/empty_cart.dart';
 
@@ -13,102 +14,115 @@ class ShoppingCart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    GlobalMethods globalMethods = GlobalMethods();
     final cartProvider = Provider.of<CartProvider>(context);
 
     return cartProvider.getCartItems.isEmpty
         ? Scaffold(body: EmptyCart())
         : Scaffold(
-        bottomSheet: checkoutSection(context),
-        appBar: AppBar(
-          title: Text('Cart item'),
-          actions: [
-            InkWell(
-              onTap: (){},
-              child: Icon(Icons.delete),
-            ),
-          ],
-        ),body: Container(
-      margin: EdgeInsets.only(bottom: 60),
-          child: ListView.builder(
-              itemCount: cartProvider.getCartItems.length,
-      itemBuilder: (BuildContext ctx, int index){
-        return ChangeNotifierProvider.value(
-          value: cartProvider.getCartItems.values.toList()[index],
-          child: CartFull(
-            productId:
-            cartProvider.getCartItems.keys.toList()[index],
-            // id:  cartProvider.getCartItems.values.toList()[index].id,
-            // productId: cartProvider.getCartItems.keys.toList()[index],
-            // price: cartProvider.getCartItems.values.toList()[index].price,
-            // title: cartProvider.getCartItems.values.toList()[index].title,
-            // imageUrl: cartProvider.getCartItems.values.toList()[index].imageUrl,
-            // quatity: cartProvider.getCartItems.values.toList()[index].quantity,
-          ),
-        );
-      }),
-        )
-    );
-  }
-  Widget checkoutSection(BuildContext ctx){
-    return Container(
-      decoration: BoxDecoration(
-        border: Border(top: BorderSide(
-          color: Colors.grey,
-          width: .5,
-        ),),
-      ),
-      child:
-      Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-            children: [
-          Expanded(
-            flex: 2,
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30),
-                gradient: LinearGradient(
-                    colors: [
-                      ColorsConsts.gradiendLStart,
-                      ColorsConsts.gradiendLEnd ],
-                    stops: [
-                      0.0,
-                      .7]
-
+            bottomSheet: checkoutSection(context, cartProvider.totalamount),
+            appBar: AppBar(
+              title: Text('Cart (${cartProvider.getCartItems.length})'),
+              actions: [
+                InkWell(
+                  onTap: () {
+                    globalMethods.showDialogg(
+                        'Clear cart',
+                        'Your cart will be cleared!',
+                        () => cartProvider.clearCart(),
+                        context);
+                    // cartProvider.clearCart();
+                  },
+                  child: Icon(Icons.delete),
                 ),
-              ),
-              child: Material(
-                borderRadius: BorderRadius.circular(30),
-                color: Colors.transparent,
-                child: InkWell(
-                  borderRadius: BorderRadius.circular(30),
-                  onTap: (){},
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text("Checkout",
-                      style: TextStyle(
-                          color: Theme.of(ctx).textSelectionColor,
-                          fontSize: 18,
-                          fontWeight: FontWeight.w600),),
+              ],
+            ),
+            body: Container(
+              margin: EdgeInsets.only(bottom: 60),
+              child: ListView.builder(
+                  itemCount: cartProvider.getCartItems.length,
+                  itemBuilder: (BuildContext ctx, int index) {
+                    return ChangeNotifierProvider.value(
+                      value: cartProvider.getCartItems.values.toList()[index],
+                      child: CartFull(
+                        productId:
+                            cartProvider.getCartItems.keys.toList()[index],
+                        // id:  cartProvider.getCartItems.values.toList()[index].id,
+                        // productId: cartProvider.getCartItems.keys.toList()[index],
+                        // price: cartProvider.getCartItems.values.toList()[index].price,
+                        // title: cartProvider.getCartItems.values.toList()[index].title,
+                        // imageUrl: cartProvider.getCartItems.values.toList()[index].imageUrl,
+                        // quatity: cartProvider.getCartItems.values.toList()[index].quantity,
+                      ),
+                    );
+                  }),
+            ));
+  }
+
+  Widget checkoutSection(BuildContext ctx, double subtotal) {
+    return Container(
+        decoration: BoxDecoration(
+          border: Border(
+            top: BorderSide(
+              color: Colors.grey,
+              width: .5,
+            ),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              Expanded(
+                flex: 2,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    gradient: LinearGradient(colors: [
+                      ColorsConsts.gradiendLStart,
+                      ColorsConsts.gradiendLEnd
+                    ], stops: [
+                      0.0,
+                      .7
+                    ]),
+                  ),
+                  child: Material(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Colors.transparent,
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(30),
+                      onTap: () {},
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          "Checkout",
+                          style: TextStyle(
+                              color: Theme.of(ctx).textSelectionColor,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600),
+                        ),
+                      ),
+                    ),
                   ),
                 ),
               ),
-            ),
+              Spacer(),
+              Text(
+                "Total: ",
+                style: TextStyle(
+                    color: Theme.of(ctx).textSelectionColor,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600),
+              ),
+              Text(
+                "KES. ${subtotal.toStringAsFixed(2)}",
+                style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500),
+              ),
+            ],
           ),
-          Spacer(),
-          Text("Total: ",
-            style: TextStyle(
-                color: Theme.of(ctx).textSelectionColor,
-                fontSize: 18,
-                fontWeight: FontWeight.w600),),
-          Text("KES. 123.00",
-            style: TextStyle(
-                color: Colors.blue,
-                fontSize: 18,
-                fontWeight: FontWeight.w500),),
-        ],
-        ),
-      )
-    );
+        ));
   }
 }
