83654310ffa8b26037d87ff90864292b0643aca8
kiiru4reals <smaina991@gmail.com>
2021-11-19 10:41:22 +0300

    code formatting and optimisation

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,27 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:shop/auth/login.dart';
-import 'package:shop/auth/sign_up.dart';
 import 'package:shop/const/theme_data.dart';
-import 'package:shop/provider/cart_provider.dart';
-import 'package:shop/provider/favs_provider.dart';
 import 'package:shop/routes/product_details.dart';
 import 'package:shop/provider/dark_theme_provider.dart';
 import 'package:shop/provider/products.dart';
-import 'package:shop/routes/upload_new_product.dart';
-import 'package:shop/ui/bottom_bar.dart';
-import 'package:shop/ui/landing_page.dart';
 import 'package:shop/ui/wishlist.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shop/routes/brands_navigation_rail.dart';
-import 'package:shop/routes/categories_feeds.dart';
+import 'routes/brands_navigation_rail.dart';
+import 'routes/categories_feeds.dart';
+import 'routes/upload_new_product.dart';
+import 'provider/cart_provider.dart';
+import 'provider/favs_provider.dart';
+import 'auth/login.dart';
+import 'auth/sign_up.dart';
+import 'package:shop/ui/bottom_bar.dart';
 import 'package:shop/ui/cart.dart';
 import 'package:shop/ui/feeds.dart';
+import 'package:shop/ui/landing_page.dart';
+import 'package:shop/ui/main_screen.dart';
+import 'package:shop/ui/user_state.dart';
 
-Future<void> main() async {
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -42,45 +43,70 @@ class _MyAppState extends State<MyApp> {
     getCurrentAppTheme();
     super.initState();
   }
+
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-        providers: [
-          ChangeNotifierProvider(create: (_) {
-            return themeChangeProvider;
-          }),
-          ChangeNotifierProvider(create: (_) =>
-            Products(),
-          ),
-          ChangeNotifierProvider(
-            create: (_) => CartProvider(),
-          ),
-          ChangeNotifierProvider(
-            create: (_) => FavsProvider(),
-          ),
-        ],
-        child:
-        Consumer<DarkThemeProvider>(builder: (context, themeData, child) {
-          return MaterialApp(
-            title: 'Flutter Demo',
-            theme: Styles.themeData(themeChangeProvider.darkTheme, context),
-            home: LandingPage(),
-            //initialRoute: '/',
-            routes: {
-              //   '/': (ctx) => LandingPage(),
-              BrandNavigationRailScreen.routeName: (ctx) =>
-                  BrandNavigationRailScreen(),
-              ShoppingCart.routeName: (ctx) => ShoppingCart(),
-              Feeds.routeName: (ctx) => Feeds(),
-              WishList.routeName: (ctx) => WishList(),
-              ProductDetails.routeName: (ctx) => ProductDetails(),
-              CategoriesFeedsScreen.routeName: (ctx) => CategoriesFeedsScreen(),
-              LoginScreen.routeName: (ctx) => LoginScreen(),
-              SignUpScreen.routeName: (ctx) => SignUpScreen(),
-              BottomBarScreen.routeName: (ctx) => BottomBarScreen(),
-              UploadProductForm.routeName: (ctx) => UploadProductForm()
-            },
-          );
-        }));
+    return FutureBuilder<Object>(
+        future: _initialization,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return MaterialApp(
+              home: Scaffold(
+                body: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              ),
+            );
+          } else if (snapshot.hasError) {
+            MaterialApp(
+              home: Scaffold(
+                body: Center(
+                  child: Text('Error occured'),
+                ),
+              ),
+            );
+          }
+          return MultiProvider(
+              providers: [
+                ChangeNotifierProvider(create: (_) {
+                  return themeChangeProvider;
+                }),
+                ChangeNotifierProvider(
+                  create: (_) => Products(),
+                ),
+                ChangeNotifierProvider(
+                  create: (_) => CartProvider(),
+                ),
+                ChangeNotifierProvider(
+                  create: (_) => FavsProvider(),
+                ),
+              ],
+              child: Consumer<DarkThemeProvider>(
+                  builder: (context, themeData, child) {
+                    return MaterialApp(
+                      title: 'Flutter Demo',
+                      theme:
+                      Styles.themeData(themeChangeProvider.darkTheme, context),
+                      home: UserState(),
+                      //initialRoute: '/',
+                      routes: {
+                        //   '/': (ctx) => LandingPage(),
+                        BrandNavigationRailScreen.routeName: (ctx) =>
+                            BrandNavigationRailScreen(),
+                        ShoppingCart.routeName: (ctx) => ShoppingCart(),
+                        Feeds.routeName: (ctx) => Feeds(),
+                        WishList.routeName: (ctx) => WishList(),
+                        ProductDetails.routeName: (ctx) => ProductDetails(),
+                        CategoriesFeedsScreen.routeName: (ctx) =>
+                            CategoriesFeedsScreen(),
+                        LoginScreen.routeName: (ctx) => LoginScreen(),
+                        SignUpScreen.routeName: (ctx) => SignUpScreen(),
+                        BottomBarScreen.routeName: (ctx) => BottomBarScreen(),
+                        UploadProductForm.routeName: (ctx) => UploadProductForm(),
+                      },
+                    );
+                  }));
+        });
   }
 }
